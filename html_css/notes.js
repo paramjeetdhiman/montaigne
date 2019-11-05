7fe23861f4d5b5c24fa49007466b2b79108cd4ef
Paramjeet Dhiman <paramjeetdhiman1997@gmail.com>
2019-11-05 17:27:27 +0530

    first commit

---
+++ html_css/notes.js
@@ -0,0 +1,26 @@
+    /******
+     * HTML
+     * -----
+     * 
+     * Semantic elements = elements with a meaning.
+     * Semantic elements: div, span
+     * Non-semantic elements: form, table, artice
+     * 
+     * 
+     * 
+     * -------------------------------
+     * CSS
+     * -----
+     * 
+     * attribute selector: [class *="col-"]{...}
+     * 
+     * 
+     * 
+     * 
+     * How Web Browser works?
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
